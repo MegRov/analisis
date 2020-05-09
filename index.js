4f8cb6436f560787838f7e9b0f07994ea31af224
Miguel Guevara <lxmxg.96@outlook.com>
2020-05-09 12:47:50 -0500

    Añadir vistas productos

--- index.js
+++ index.js
@@ -1,16 +1,44 @@
-$(document).ready(function(){//Cambiar vista de saludo a gastos fijos
+$(document).ready(function(){ //Cambiar vista de saludo a gastos fijos
 
     $('#ocultarsaludo').click(function(){
 
         $("#saludo").hide(2000);
         $("#gastos-fijos").show(2000);
     });
-});
-$(document).ready(function(){//Cambiar vista de gastos fijos a productos
-
-    $('#ocultargastos-fijos').click(function(){
-
+    $('#ocultargastos-fijos').click(function(){//Cambiar vista de gastos fijos a productos
+    
         $("#gastos-fijos").hide(2000);
         $("#productos").show(2000);
     });
+    $('#moreproducts').click(function(){//Añadir productos
+
+    
+        $("#producto1").hide(21334);
+        $("#producto2").show(2000);
+        $("#productonum").html('Añade tu producto 2');
+            
+            $('#moreproducts').click(function(){
+                $("#productonum").hide(2000);                
+                $("#productonum").html('Añade tu producto 3 ');
+                $("#productonum").show(2000);
+                $("#producto2").hide(21334);
+                $("#producto3").show(2000);
+                
+                    
+                    $('#moreproducts').click(function(){
+                        $("#productonum").hide(2000);                
+                        $("#productonum").html('Añade tu producto 4');
+                        $("#productonum").show(2000);
+                        $("#producto3").hide(21334);
+                        $("#producto4").show(2000);
+                        $("#moreproducts").hide(2342);
+                        
+                        
+                        
+                            });
+                    });
+        
+
+    });
 });
+
